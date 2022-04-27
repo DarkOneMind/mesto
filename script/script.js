4bcacd2cf00f784f101f7d333b5b6731c9b7da74
Oleg Dobrydnev <FirstDarkMind@yandex.ru>
2022-04-27 20:29:24 +0700

    исправлены ошибки первой попытки

--- script/script.js
+++ script/script.js
@@ -1,12 +1,12 @@
 let profileName = document.querySelector('.profile__title');
-let editButton = document.querySelector('.profile__edit-button');
 let profilePersonalInfo = document.querySelector('.profile__subtitle');
-let popup = document.querySelector('.popup');
-let closeButton = document.querySelector('.popup__close');
-let formElement = document.querySelector('.popup__form');
-let editName = document.querySelector('.popup__input_type_name');
-let editPersonalInfo = document.querySelector('.popup__input_type_personal');
-let saveButton = document.querySelector('.popup__save');
+const editButton = document.querySelector('.profile__edit-button');
+const popup = document.querySelector('.popup');
+const closeButton = document.querySelector('.popup__close');
+const formElement = document.querySelector('.popup__form');
+const editName = document.querySelector('.popup__input_type_name');
+const editPersonalInfo = document.querySelector('.popup__input_type_personal');
+const saveButton = document.querySelector('.popup__save');
 
 function openPopup() {
   popup.classList.add('popup_opened');
@@ -18,9 +18,6 @@ function closePopup() {
   popup.classList.remove('popup_opened');
 }
 
-editButton.addEventListener('click', openPopup);
-closeButton.addEventListener('click', closePopup);
-
 function formSubmitHandler (evt) {
   evt.preventDefault();
   profileName.textContent = editName.value;
@@ -28,4 +25,6 @@ function formSubmitHandler (evt) {
   closePopup();
 }
 
+editButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
 formElement.addEventListener('submit', formSubmitHandler);
